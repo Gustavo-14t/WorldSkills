4ae79f74d37cf2c437fbc560e859d0ad361d822b
Gustavo <garamujo07@gmail.com>
2025-04-24 13:42:49 -0300

    first commit

---
+++ src/main/java/com/mycompany/wordskill/WordSkill.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.wordskill;
+
+/**
+ *
+ * @author devmat
+ */
+public class WordSkill {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
